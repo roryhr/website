42884fb9b65972a7a4af6d75d16ecf6b958153b4
Rory Hartong-Redden <roryhr@gmail.com>
2019-12-14 13:33:11 -0800

    Donuts linked to website!

---
+++ content/images/donuts/generate_map.js
@@ -0,0 +1,36 @@
+var xmlhttp = new XMLHttpRequest();
+
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var donutShop = JSON.parse(this.responseText);
+    } else {
+        return false
+    }
+    L.geoJSON(donutShop, {
+        onEachFeature: onEachFeature
+    }).addTo(map);
+};
+
+
+var map = L.map('map').setView([37.28099, -121.95252], 7);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.light'
+}).addTo(map);
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent
+        && typeof(feature.geometry.coordinates[0] == "number")) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+
+xmlhttp.open("GET",
+    url="https://raw.githubusercontent.com/roryhr/code/master/donut_reviews/output/map_data.json",
+    async=true);
+xmlhttp.send();
