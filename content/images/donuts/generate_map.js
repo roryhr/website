75f7b57ca7b2a332141fa5ea7a1e9d94c1779c8c
Rory Hartong-Redden <roryhr@gmail.com>
2021-05-10 20:45:10 -0600

    fix donuts

--- content/images/donuts/generate_map.js
+++ content/images/donuts/generate_map.js
@@ -12,14 +12,15 @@ xmlhttp.onreadystatechange = function() {
 };
 
 
-var map = L.map('map').setView([40.5092065,-60.4475978], 3);
+var map = L.map('mapid').setView([37.910,-119.0117], 4);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicm9yeWhyIiwiYSI6ImNrb2pkbnA2dzA1ZDMydnJ2ejJmNmVqejEifQ.Mz78X_orR5CwkLWGgFgDng', {
     maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.light'
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
 }).addTo(map);
 
 function onEachFeature(feature, layer) {
