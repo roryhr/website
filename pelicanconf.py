4d2d7ebe17394241b37b4f0ddd070208c1501553
Rory Hartong-Redden <rory.hartong-redden@fastradius.com>
2023-10-06 09:45:54 -0600

    cycling climbs too

--- pelicanconf.py
+++ pelicanconf.py
@@ -52,7 +52,7 @@ SOCIAL = [
 DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 # Turn off Feed generation
 FEED_ALL_ATOM = None
